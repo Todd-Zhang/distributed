d982de78ff86c8366ef259c435b123418075c96a
zhangtao <@>
2019-11-08 10:40:31 +0800

    first commit

---
+++ src/test/java/com/todd/zookeeper/distributed/DistributedLocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todd.zookeeper.distributed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
